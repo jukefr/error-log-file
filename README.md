dd7c775687c75963346f5fd73837a423752696be
Kevin Jullien <hi@juke.fr>
2018-12-13 17:16:41 +0100

    first mvp

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # error.log
 a package to log errors directly to a error.log file with helpers
+
+## usage
+```javascript
+const log = require('error.log')
+
+await log('something went wrong')
+
+// Optionnal arguments
+await log('there was an error', {fileName: 'error.txt'})
+await log('there was an error', {date: new Date('2012')})
+```
--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log(123)
+import { writeFile } from "fs";
+
+const writeFileP = (fileName: string, data: string): Promise<void> =>
+  new Promise<void>((resolve, reject) =>
+    writeFile(fileName, data, err => (err ? reject(err) : resolve()))
+  );
+
+export = async (
+  log: string,
+  {
+    fileName = "error.log",
+    date = new Date()
+  }: { fileName?: string; date?: Date } = {}
+): Promise<void> =>
+  writeFileP(
+    fileName,
+    `[${date.getFullYear()}-${date.getMonth()}-${date.getDate()}|${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}] ${log}`
+  );
